1dcb9be6eed7d3f944e93d14610e24e9e9f65557
ekpeianthony <anthony4arsenal@gmail.com>
2022-12-02 10:23:28 +0100

    working

--- app/src/main/java/cub360/sys/devotionalapp/presentation/Splashscreen/Splash_Screen.kt
+++ app/src/main/java/cub360/sys/devotionalapp/presentation/Splashscreen/Splash_Screen.kt
@@ -3,14 +3,18 @@ package cub360.sys.devotionalapp.presentation.Splashscreen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cub360.sys.devotionalapp.R
@@ -20,7 +24,7 @@ fun SplashScreen(){
   Items(
       painterResource(id = R.drawable.ebele),
       headerTitle = "Start Your Day With\n a Reading Plan",
-      headerDescription = "365 days reading plan that would help strenghten your spirit for the year",
+      headerDescription = "365 days reading plan that would help strenghten your spirit life for the year that keeps us in the word daily",
       index = 0,
 
   )
@@ -54,14 +58,59 @@ fun Items(
 
       )
 
-      Spacer(modifier = Modifier.height(10.dp))
+      Spacer(modifier = Modifier.height(7.dp))
 
 
       Text(
           text = headerTitle,
-          fontWeight = FontWeight.W500,
+          fontWeight = FontWeight.W400,
           fontSize = 25.sp,
+          textAlign = TextAlign.Center
 
           )
+
+      Spacer(modifier = Modifier.height(7.dp))
+
+      Text(
+          text = headerDescription,
+          fontWeight = FontWeight.W300,
+          fontSize = 18.sp,
+          textAlign = TextAlign.Center
+
+      )
+
+      Spacer(modifier = Modifier.height(77.dp))
+
+
+      NextButton()
+
+
   }
+}
+
+
+/** next Buttons **/
+@Composable
+fun NextButton(){
+    Card(
+        shape = RoundedCornerShape(5.dp),
+        elevation = 4.dp,
+        backgroundColor = Color.Blue,
+        modifier = Modifier
+            .width(180.dp)
+            .height(50.dp)
+
+
+    ) {
+        Text(
+            text = "Next",
+            fontWeight = FontWeight.W400,
+            fontSize = 18.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(10.dp)
+
+
+        )
+    }
 }
